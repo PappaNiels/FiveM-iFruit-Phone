7dba9b1b9bd42a5ec521913cf532d2748d8e4071
pindakaastosti <pindakaastosti111@gmail.com>
2022-08-04 21:27:56 +0200

    feat(contacts) standard gta contacts added

--- src/contacts.lua
+++ src/contacts.lua
@@ -1,24 +1,75 @@
 contacts = {
-    {1}
+    {2}
 }
 
-local txd = {
-    "CHAR_MULTIPLAYER"
+assistant = "char_pa_female" -- char_pa_male
+enableStandardContacts = true
+
+standardContacts = { -- char_detonatebomb
+    {"Agent 14", "char_agent14"},
+    {"Assistant", assistant},
+    {"Brucie", "char_mp_brucie"},
+    {"Bryony", "char_bryony"},
+    {"Captian", "char_boatsite2"},
+    {"Chastity", "char_stripper_chastity"},
+    {"Cheetah", "char_stripper_cheetah"},
+    {"Dom", "char_dom"},
+    {"Downtown Cab Co.", "char_taxi"},
+    {"Emergency Services", "char_call911"},
+    {"English Dave", "char_english_dave"},
+    {"Franklin", "char_fixfranklin"},
+    {"Fufu", "char_stripper_fufu"},
+    {"Gerald", "char_mp_gerald"},
+    {"Imani", "char_imani"},
+    {"Infernus", "char_stripper_infernus"},
+    {"Juliet", "char_stripper_juliet"},
+    {"Lamar", "char_lamar"},
+    {"Lazlow", "char_lazlow2"},
+    {"Lester", "char_lester"},
+    {"LJT", "char_ljt"},
+    {"Malc", "char_malc"},
+    {"Martin", "char_martin"},
+    {"Mechanic", "char_ls_customs"},
+    {"Merryweather", "char_mp_merryweather"},
+    {"Mors Mutual Insurance", "char_mp_mors_mutual"},
+    {"Ms. Baker", "char_casino_manager"},
+    {"Nikki", "char_stripper_nikki"},
+    {"Paige", "char_paige"},
+    {"Pavel", "char_pavel"},
+    {"Peach", "char_stripper_peach"},
+    {"Pegasus", "char_pegasus_delivery"},
+    {"Ron", "char_ron"},
+    {"Sapphire", "char_stripper_sapphire"},
+    {"Simeon", "char_simeon"},
+    {"Tony", "char_tony_prince"},
+    {"Wendy", "char_wendy"},
+    --{"Yohan", "char_yohan"},
+    {"Yung Ancestor", "char_tw"}
 }
 
 local function LoadPlayerContacts()
-    LoadTexture(txd[1])
-    for i = 1, #contacts do 
-        BeginScaleformMovieMethod(scaleform, "SET_DATA_SLOT")
-        ScaleformMovieMethodAddParamInt(2)
-        ScaleformMovieMethodAddParamInt(i - 1)
-        ScaleformMovieMethodAddParamInt(0) -- SYSTEM::TO_FLOAT(0), 0f, -1f, -1f, "CELL_7001", &(Global_1998[Global_7451 /*29*/].f_3), &(Global_1998[Global_7451 /*29*/].f_7), 0, 0
-        --BeginTextCommandScaleformString("CELL_7003")
-        --EndTextCommandScaleformString()
-        ScaleformMovieMethodAddParamPlayerNameString("NIELS") -- Text
-        ScaleformMovieMethodAddParamPlayerNameString("test")
-        ScaleformMovieMethodAddParamTextureNameString(txd[1]) -- Profile picture
-        EndScaleformMovieMethod()
+    LoadTexture("char_default")
+    for i = 1, #contacts + #standardContacts do 
+        if i <= #contacts then 
+            BeginScaleformMovieMethod(scaleform, "SET_DATA_SLOT")
+            ScaleformMovieMethodAddParamInt(2) -- Type
+            ScaleformMovieMethodAddParamInt(i - 1) -- Position
+            ScaleformMovieMethodAddParamBool(false)  -- !
+            ScaleformMovieMethodAddParamPlayerNameString(GetName(contacts[i][1])) -- Text
+            ScaleformMovieMethodAddParamTextureNameString("char_default") -- Profile picture
+            ScaleformMovieMethodAddParamTextureNameString("char_default") -- Profile picture
+            EndScaleformMovieMethod()
+        elseif i > #contacts and enableStandardContacts then
+            LoadTexture(standardContacts[i - #contacts][2]) 
+            BeginScaleformMovieMethod(scaleform, "SET_DATA_SLOT")
+            ScaleformMovieMethodAddParamInt(2) -- Type
+            ScaleformMovieMethodAddParamInt(i - 1) -- Position
+            ScaleformMovieMethodAddParamBool(false)  -- !
+            ScaleformMovieMethodAddParamPlayerNameString(standardContacts[i - #contacts][1]) -- Text
+            ScaleformMovieMethodAddParamTextureNameString(standardContacts[i - #contacts][2]) -- Profile picture
+            ScaleformMovieMethodAddParamTextureNameString(standardContacts[i - #contacts][2]) -- Profile picture
+            EndScaleformMovieMethod()
+        end
     end
 end
 
@@ -28,16 +79,24 @@ local function UnloadContacts()
     EndScaleformMovieMethod()
 end
 
-local function AddContact()
+local function AddContact(id, texture)
     UnloadContacts()
 
     for i = #contacts, 1, -1 do 
         contacts[i + 1] = contacts[i]
     end
 
-    contacts[1] = {sender, title, msg, hour, minute}
+    contacts[1] = {contact, texture}
 
     PlaySoundFrontend(-1, "Notification", "Phone_SoundSet_Michael", true)
+end
+
+local function AddBomb() -- To do
+    UnloadContacts()
+end
+
+local function RemoveBomb() -- To do
+    UnloadContacts()
 end
 
 function OpenContacts()
@@ -50,3 +109,10 @@ function OpenContacts()
 end
 
 RegisterNetEvent("cl:ifruit:addContact", AddContact)
+RegisterNetEvent("cl:ifruit:addBomb", AddBomb)
+RegisterNetEvent("cl:ifruit:removeBomb", RemoveBomb)
+
+function ScaleformString(txt)
+    BeginTextCommandScaleformString(txt)
+    EndTextCommandScaleformString()
+end
